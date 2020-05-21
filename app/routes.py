71b3950b0dd34a1c146b3646d79dba64baecf699
Haley Morgan <haley.morgan@bostonfusion.com>
2020-05-21 17:29:37 -0400

    format heading area with logos and links

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/gmf_website')
+def gmf_website():
+  return redirect("https:///www.garrettmillerfitness.com")
+
+@app.route('/forge_website')
+def forge_website():
+  return redirect("https://forgesupps.com/?utm_source=Gbaby&utm_campaign=affiliate")
 
 
